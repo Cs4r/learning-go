d2e1a109c71adfc6753a5cfd8e25e5e4cec666a1
César Aguilera <cesar.aguilera.p@gmail.com>
2017-05-23 22:36:00 +0100

    added pangram.go

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,28 @@
 package pangram
 
+import (
+	"strings"
+)
+
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(input string) bool {
+
+	if len(input) < 26 {
+		return false
+	}
+
+	occurrences := make(map[int32]int)
+
+	for _, c := range strings.ToLower(input) {
+		occurrences[c] += 1
+	}
+
+	for _, c := range "abcdefghijklmnopqrstuvwxyz" {
+		if occurrences[c] == 0 {
+			return false
+		}
+	}
+
+	return true
+}
